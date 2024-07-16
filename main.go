700371a717b270770397fb6e526ed0b0fb57cbb0
hokwach <hezronokwach@gmail.com>
2024-07-16 14:55:32 +0300

    Add HandleFunc for the error page

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ func main() {
 		fmt.Println("<Usage>", "<go run .> or <go run main.go>")
 		os.Exit(1)
 	}
-
 	http.HandleFunc("/", handlers.Request)
 	http.HandleFunc("/ascii-art", handlers.Post)
+	http.HandleFunc("/error", handlers.ErrorHandler)
+
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/",http.StripPrefix("/static/",fs))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	fmt.Println("Server is starting on http://localhost:8090")
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
